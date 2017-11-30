9ea87b16f343f05a811f9e0e596a875962cd3b6e
CMAREQ <temomareq@gmail.com>
2017-11-30 02:16:21 -0600

    Correcion en html

---
+++ Huella Ecologica/parte1.php
@@ -0,0 +1,122 @@
+<?php
+session_start();
+
+$respuesta1 = $_POST['ans1'];
+$respuesta2 = $_POST['ans2'];
+$respuesta3 = $_POST['ans3'];
+$respuesta4 = $_POST['ans4'];
+$respuesta5 = $_POST['ans5'];
+$respuesta6 = $_POST['ans6'];
+$respuesta7 = $_POST['ans7'];
+$respuesta8 = $_POST['ans8'];
+$respuesta9 = $_POST['ans9'];
+$respuesta10 = $_POST['ans10'];
+// $respuesta11 = $_POST['ans11'];
+// $respuesta12 = $_POST['ans12'];
+// $respuesta13 = $_POST['ans13'];
+$respuesta14 = $_POST['ans14'];
+
+switch($respuesta1){
+    case 0: echo 1000; break;
+    case 1: echo 2000; break;
+    case 2 : echo 3000; break;
+    case 3: echo 4000; break;
+    case 4: echo 5000; break;
+}
+switch ($respuesta2) {
+    case 0: echo 12; break;
+    case 1: echo 12; break;
+    case 2: echo 12; break;
+    case 3: echo 12; break;
+    case 4: echo 12; break;
+}
+switch ($respuesta3) {
+    case 0: echo 12; break;
+    case 1: echo 12; break;
+    case 2: echo 12; break;
+    case 3: echo 12; break;
+    case 4: echo 12; break;
+}
+switch ($respuesta4) {
+    case 0: echo 12; break;
+    case 1: echo 12; break;
+    case 2: echo 12; break;
+    case 3: echo 12; break;
+    case 4: echo 12; break;
+}
+switch ($respuesta5) {
+    case 0: echo 12; break;
+    case 1: echo 12; break;
+    case 2: echo 12; break;
+    case 3: echo 12; break;
+    case 4: echo 12; break;
+}
+switch ($respuesta6) {
+    case 0: echo 12; break;
+    case 1: echo 12; break;
+    case 2: echo 12; break;
+    case 3: echo 12; break;
+}
+switch ($respuesta7) {
+    case 0: echo 12; break;
+    case 1: echo 12; break;
+    case 2: echo 12; break;
+    case 3: echo 12; break;
+    case 4: echo 12; break;
+}
+switch ($respuesta8) {
+    case 0: echo 12; break;
+    case 1: echo 12; break;
+    case 2: echo 12; break;
+    case 3: echo 12; break;
+    case 4: echo 12; break;
+}
+switch ($respuesta9) {
+    case 0: echo 12; break;
+    case 1: echo 12; break;
+    case 2: echo 12; break;
+    case 3: echo 12; break;
+    case 4: echo 12; break;
+}
+switch ($respuesta10) {
+    case 0: echo 12; break;
+    case 1: echo 12; break;
+    case 2: echo 12; break;
+    case 3: echo 12; break;
+    case 4: echo 12; break;
+}
+// switch ($respuesta11) {
+//     case 0: echo 12; break;
+//     case 1: echo 12; break;
+//     case 2: echo 12; break;
+//     case 3: echo 12; break;
+//     case 4: echo 12; break;
+// }
+// switch ($respuesta12) {
+//     case 0: echo 12; break;
+//     case 1: echo 12; break;
+//     case 2: echo 12; break;
+//     case 3: echo 12; break;
+//     case 4: echo 12; break;
+// }
+// switch ($respuesta13) {
+//     case 0: echo 12; break;
+//     case 1: echo 12; break;
+//     case 2: echo 12; break;
+//     case 3: echo 12; break;
+//     case 4: echo 12; break;
+// }
+switch ($respuesta14) {
+    case 0: echo 12; break;
+    case 1: echo 12; break;
+    case 2: echo 12; break;
+    case 3: echo 12; break;
+    case 4: echo 12; break;
+}
+
+
+echo $respuesta2;
+
+
+
+//~ header('location: ./parte2-3.html');
---
+++ Huella Ecologica/parte4.php
@@ -0,0 +1,97 @@
+<?php
+session_start();
+
+// $respuesta1 = $_POST['ans1'];
+$respuesta2 = $_POST['ans2'];
+$respuesta3 = $_POST['ans3'];
+$respuesta4 = $_POST['ans4'];
+$respuestaTv = $_POST['ansTv'];
+$respuestaPc = $_POST['ansPc'];
+$respuestaDvd = $_POST['ansDvd'];
+$respuestaEst = $_POST['ansEst'];
+$respuesta6 = $_POST['ans6'];
+$respuesta7 = $_POST['ans7'];
+$respuesta8 = $_POST['ans8'];
+$respuesta9 = $_POST['ans9'];
+
+
+// switch($respuesta1){
+//     case 0: echo 1000; break;
+//     case 1: echo 2000; break;
+//     case 2 : echo 3000; break;
+//     case 3: echo 4000; break;
+//     case 4: echo 5000; break;
+// }
+switch ($respuesta2) {
+    case 0: echo 12; break;
+    case 1: echo 12; break;
+    case 2: echo 12; break;
+    case 3: echo 12; break;
+    case 4: echo 12; break;
+}
+switch ($respuesta3) {
+    case 0: echo 12; break;
+    case 1: echo 12; break;
+    case 2: echo 12; break;
+    case 3: echo 12; break;
+    case 4: echo 12; break;
+}
+switch ($respuesta4) {
+    case 0: echo 12; break;
+    case 1: echo 12; break;
+}
+switch ($respuestaTv) {
+    case 0: echo 12; break;
+    case 1: echo 12; break;
+    case 2: echo 12; break;
+    case 3: echo 12; break;
+    case 4: echo 12; break;
+}
+switch ($respuestaPc) {
+    case 0: echo 12; break;
+    case 1: echo 12; break;
+    case 2: echo 12; break;
+    case 3: echo 12; break;
+    case 4: echo 12; break;
+}
+switch ($respuestaDvd) {
+    case 0: echo 12; break;
+    case 1: echo 12; break;
+    case 2: echo 12; break;
+    case 3: echo 12; break;
+    case 4: echo 12; break;
+}
+switch ($respuestaEst) {
+    case 0: echo 12; break;
+    case 1: echo 12; break;
+    case 2: echo 12; break;
+    case 3: echo 12; break;
+    case 4: echo 12; break;
+}
+switch ($respuesta6) {
+    case 0: echo 12; break;
+    case 1: echo 12; break;
+    case 2: echo 12; break;
+}
+switch ($respuesta7) {
+    case 0: echo 12; break;
+    case 1: echo 12; break;
+    case 2: echo 12; break;
+    case 3: echo 12; break;
+}
+switch ($respuesta8) {
+    case 0: echo 12; break;
+    case 1: echo 12; break;
+}
+switch ($respuesta9) {
+    case 0: echo 12; break;
+    case 1: echo 12; break;
+    case 2: echo 12; break;
+    case 3: echo 12; break;
+}
+
+echo $respuesta2;
+
+
+
+//~ header('location: ./parte2-3.html');
